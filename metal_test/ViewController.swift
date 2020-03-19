6b5f60a74f7025fabc772508a3c2054eaec60aa0
Roman Derkach <pupsinatoriaroman@gmail.com>
2020-03-19 09:22:00 +0200

    Initial Commit

---
+++ metal_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  metal_test
+//
+//  Created by Roman Derkach on 19.03.2020.
+//  Copyright © 2020 Roman Derkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
