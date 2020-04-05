718cc9dee32260295725df201940147869350099
Roman Derkach <pupsinatoriaroman@gmail.com>
2020-04-05 18:16:12 +0300

    added second zombie

--- metal_test/Graphics/Metal/engine/Scene.swift
+++ metal_test/Graphics/Metal/engine/Scene.swift
@@ -12,12 +12,13 @@ class Scene: Node {
     }
     
     func render(commandEncoder: MTLRenderCommandEncoder, deltaTime: Float) {
+        update(deltaTime: deltaTime)
         let viewMatrix = matrix_float4x4(translationX: 0, y: 0, z: -4)
         for child in children {
             child.render(commandEncoder: commandEncoder, parentModelViewMatrix: viewMatrix)
         }
     }
-    
+    func update(deltaTime: Float) {}
 }
 
 class GameScene: Scene {
@@ -27,6 +28,15 @@ class GameScene: Scene {
         quad = Plane(device: device, imageName: "picture.png")
         super.init(device: device, size: size)
         add(childNode: quad)
-        
+        quad.position.x = -1
+        quad.position.y = 1
+        let quad2 = Plane(device: device, imageName: "picture.png")
+        quad2.position.x = 1
+        quad2.position.y = -1
+        add(childNode: quad2)
+    }
+    
+    override func update(deltaTime: Float) {
+        quad.rotation.y += deltaTime
     }
 }
